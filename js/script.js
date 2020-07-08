826fac38a3b849463e70f6f630572813301dff1e
Alex <al.kaminskyi@gmail.com>
2020-07-08 20:23:06 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use sсtrict";
+
+// alert(Hello);
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18")
+// console.log(typeof(answer));
+
+
+const answers = [];
+
+// answers[0] = prompt('Как ваше имя?','');
+// answers[1] = prompt('Какая ваша фамилия?','');
+// answers[2] = prompt('Сколько вам лет?','');
+
+
+console.log(typeof(answers));
